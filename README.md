6e18c3120da4722a86ec293e44981c179226d4ba
Jonathon <jon@thon.id.au>
2017-04-01 19:11:31 +1100

    .

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 An embedded, schema-free, multi-modal database for node.js. Supports graph queries and sql queries. Persistence uses Google's leveldb. See sample.js for more information. Built for discussion purposes.
 
 ## Getting Started
+
 1. `npm install schemafree`
 2. `let db = require('schemafree')()`
 3. Review `sample.js` for a quick demo and to check everything is working
@@ -15,5 +16,25 @@ The api is very short. The source is very short too. At this stage the best plac
 - `db.unset` to remove a entity from the database
 - `db.link` to link two entities
 - `db.unlink` to unlink two entities
+- `db.search` to get an entity from the database using a predicate function
 - `db.query` to run a sql query against the database
 - `db.walk` to run a graph traversal
+
+## Queries
+
+The `query()` function supports almost all of SQL-99 including temp tables, all join types, rollup, cube, and grouping sets, In addition it supports use of a new 'link' statement which makes traversal of many-to-many relationship tables cleaner. See sample.js for more information.
+
+The `search()` function provides a simple way to quickly grab any entity from the database using a javascript function. To get all person entities who with the firstname "Barry" simple type db.search(person => person.firstname == "Barry").
+
+The `walk()` function offers a fluid syntax for quickly traversing stored data like a graph. Available steps are `all()`, `group()`, `map()`, `filter()`, `label()`, and `loop()`.
+
+## Version History
+
+- 0.2 Initial release
+- 0.3 Implemented db.unlink. Implemented the sql link shorthand. Bug fixes.
+- 0.4 Finished implementing the full suite of walk() functions. Untested.
+
+## High Level Todos
+- Write tests
+- Write documentation
+- Discuss performance optimisations
--- walk.js
+++ walk.js
@@ -19,29 +19,56 @@ module.exports = (db, start) => {
     api.execute = async () => {
         let start = api.steps.shift()
         if (start.apply) start = db[require('./args')(start)].filter(start) //start must be a predicate or an entity object
-        if(!start.splice) start = [start]
+        if (!start.splice) start = [start]
 
         let stepIndex = 0
-        let context = { comments: "Not fully implemented", result: start }
+        let context = { values: { result: start }, labels: {} }
+
         while (stepIndex < api.steps.length) {
             let step = api.steps[stepIndex]
 
             if (step.type == 'all') {
                 let newResult = []
-                context.result.forEach(item => {
-                    if(item[step.key]) newResult = newResult.concat(Object.values(item[step.key]))
+                context.values.result.forEach(item => {
+                    if (item[step.key]) newResult = newResult.concat(Object.values(item[step.key]))
                 })
-                context.result = newResult
+                context.values.result = newResult
                 stepIndex++
 
+            } else if (step.type == 'group') {
+                let newResult = {}
+                Promise.all(context.values.result.forEach(async item => {
+                    let groupName = await step.predicate(item)
+                    if (!newResult[groupName]) newResult[groupName] = []
+                    newResult[groupName].push(item)
+                }))
+                context.values.result = newResult
+                stepIndex++
+
+            } else if (step.type == 'map') {
+                context.values.result = await Promise.all(context.values.result.map(async item => await step.predicate(item)))
+                stepIndex++
+
+            } else if (step.type == 'filter') {
+                context.values.result = await Promise.all(context.values.result.filter(async item => await step.predicate(item)))
+                stepIndex++
+
+            } else if (step.type == 'label') {
+                context.labels[step.label] = stepIndex
+                context.values[step.label] = context.values.result
+                await step.predicate(context)
+                stepIndex++
+
+            } else if (step.type == 'loop') {
+                stepIndex = await step.predicate(context) ? context.labels[step.label] : stepIndex + 1
+
             } else {
-                context.result.push(api.steps[stepIndex])
+                console.log("An invalid traversal step type was found.")
                 stepIndex++
             }
         }
-        return context
+        
+        return context.values
     }
-
     return api
-
 }
